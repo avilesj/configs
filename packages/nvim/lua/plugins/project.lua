cd8842fa8b45ae76330e773aae5a16ed448bb203
Jose Aviles <javiles@mailbox.org>
2023-11-23 13:18:55 -0500

    Updated packages: nvim

--- packages/nvim/lua/plugins/project.lua
+++ packages/nvim/lua/plugins/project.lua
@@ -1,22 +1,34 @@
 return {
-  "ahmedkhalf/project.nvim",
-  lazy = false,
-  keys = {
-    {
-      "<leader>pr",
-      function()
-        require("lazy").load({ plugins = { "project_nvim" } })
-        vim.schedule(function()
-          vim.cmd("Telescope projects")
-        end)
-      end,
+  {
+    "folke/which-key.nvim",
+    optional = true,
+    opts = {
+      defaults = {
+        ["<leader>p"] = { name = "+projects" },
+      },
     },
   },
-  config = function()
-    require("project_nvim").setup({})
-    require("telescope").load_extension("projects")
-  end,
-  dependencies = {
-    "nvim-telescope/telescope.nvim",
+  {
+
+    "ahmedkhalf/project.nvim",
+    lazy = false,
+    keys = {
+      {
+        "<leader>pr",
+        function()
+          vim.schedule(function()
+            vim.cmd("Telescope projects")
+          end)
+        end,
+        desc = "recent projects",
+      },
+    },
+    config = function()
+      require("project_nvim").setup({})
+      require("telescope").load_extension("projects")
+    end,
+    dependencies = {
+      "nvim-telescope/telescope.nvim",
+    },
   },
 }

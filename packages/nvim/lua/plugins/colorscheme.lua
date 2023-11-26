c76516d2dcd34118b3a94a1e29fffa520c020e60
Jose Aviles <javiles@mailbox.org>
2023-11-25 21:18:28 -0500

    Updated packages: alacritty, dotfiles, nvim

--- packages/nvim/lua/plugins/colorscheme.lua
+++ packages/nvim/lua/plugins/colorscheme.lua
@@ -1,9 +1,15 @@
 return {
   { "rebelot/kanagawa.nvim" },
   {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+  },
+  {
     "LazyVim/LazyVim",
     opts = {
-      colorscheme = "elflord",
+      colorscheme = "tokyonight-night",
     },
   },
 }
